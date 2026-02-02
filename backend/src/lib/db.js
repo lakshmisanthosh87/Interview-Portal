53001c725866e7f87f60760b6b9299450408ee5e
lakshmisanthosh87 <lakshmisanthosh8714@gmail.com>
2026-02-02 16:09:23 +0530

    db and tools setuo

---
+++ backend/src/lib/db.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose"
+
+import {ENV} from "./env.js"
+
+
+
+export const connectDB = async()=>{
+    try {
+
+         const conn=await mongoose.connect(ENV.DB_URL)
+         console.log("db connection success:",conn.connection.host)
+        
+    } catch (error) {
+        console.error("connection error",error)
+        process.exit(1)
+        
+    }
+}
+
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import path from 'path';
 import { ENV } from './lib/env.js';
+import { connectDB } from './lib/db.js';
 
 
 
@@ -13,6 +14,8 @@ const __dirname = path.resolve();
 
 // console.log('Environment Variable PORT:', ENV.PORT);
 // console.log('Environment Variable DB_URL:', ENV.DB_URL);
+// console.log("DB_URL =", process.env.DB_URL);
+
 
 app.get('/', (req, res) => {
     res.status(200).json({ message: 'success from api' })
@@ -26,6 +29,8 @@ if (ENV.NODE_ENV === 'production') {
     })
 }
 
-app.listen(ENV.PORT, () => {
-    console.log(`Server is running on port ${ENV.PORT}`);
-});
+connectDB().then(()=>{
+    app.listen(ENV.PORT,()=>{
+        console.log("server is running on port :",ENV.PORT)
+    })
+})
