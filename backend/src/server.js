b70a41843edd16d2c8eab5a57e609cbf8898141c
lakshmisanthosh87 <lakshmisanthosh8714@gmail.com>
2026-02-03 14:34:35 +0530

    fix frontend static path for production

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,12 @@
 import express from 'express';
 import path from 'path';
 import cors from "cors"
-import {serve} from "inngest/express"
+import { serve } from "inngest/express"
 
 
 import { ENV } from './lib/env.js';
 import { connectDB } from './lib/db.js';
-import {inngest,functions} from "./lib/inngest.js"
+import { inngest, functions } from "./lib/inngest.js"
 
 
 
@@ -24,24 +24,28 @@ const __dirname = path.resolve();
 
 //middleware
 app.use(express.json())
-app.use(cors({origin:ENV.CLIENT_URL,Credentials:true}))
+app.use(cors({ origin: ENV.CLIENT_URL, Credentials: true }))
 
-app.use("/api/inngest",serve({client:inngest,functions}))
+app.use("/api/inngest", serve({ client: inngest, functions }))
 
 app.get('/', (req, res) => {
     res.status(200).json({ message: 'success from api' })
 })
 
-// male our app ready for dployment
+// make our app ready for dployment
 if (ENV.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../frontend/dist'))); 
-    app.get('/{*any}', (req, res) => {
-        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
-    })
+    app.use(express.static(path.join(__dirname, '../../frontend/dist')));
+
+    app.get('*', (req, res) => {
+        res.sendFile(
+            path.join(__dirname, '../../frontend/dist/index.html')
+        );
+    });
+
 }
 
-connectDB().then(()=>{
-    app.listen(ENV.PORT,()=>{
-        console.log("server is running on port :",ENV.PORT)
+connectDB().then(() => {
+    app.listen(ENV.PORT, () => {
+        console.log("server is running on port :", ENV.PORT)
     })
 })
