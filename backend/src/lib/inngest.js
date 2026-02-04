788ebdf9a4ee23910bafddceac78283f1acc0341
lakshmisanthosh87 <lakshmisanthosh8714@gmail.com>
2026-02-04 19:14:53 +0530

    create a stream.js

--- backend/src/lib/inngest.js
+++ backend/src/lib/inngest.js
@@ -1,6 +1,7 @@
 import {Inngest} from "inngest"
 import {connectDB} from "./db.js"
 import User from "../modules/User.js"
+import { deleteStreamUser } from "./stream.js"
 
 
 export const inngest = new Inngest({id:"interview-portal"})
@@ -21,6 +22,12 @@ const syncUser= inngest.createFunction(
         }
 
         await User.create(newUser)
+
+        await upsertStreamUser({
+            id: newUser.clerkId.toString(),
+            name: newUser.name,
+            image: newUser.profileImage,
+        })
     }
 )
 
@@ -34,6 +41,8 @@ const deleteUserFromDB= inngest.createFunction(
         const {id} = event.data
 
         await User.deleteOne({clerkId:id})
+
+        await deleteStreamUser(id.toString());
     }
 )
 
