a4229a272a4f27d9a29dcf9067901ef36bde4252
lakshmisanthosh87 <lakshmisanthosh8714@gmail.com>
2026-02-09 16:43:51 +0530

    refactoring sessionControllers

--- backend/src/controllers/sessionControllers.js
+++ backend/src/controllers/sessionControllers.js
@@ -111,9 +111,16 @@ export async function joinSession(req, res) {
         if (!session) return
         res.status(404).json({ message: "Session not found" })
 
+        if(session.status !== "active"){
+            return res.status(400).json({message:"cannot join a completed session"})
+        }
+
+        if(session.host.toString()== userId.toString()){
+            return res.status(400).json({message:"Host cannot join their own session as participant"})
+        }
         // check if session is already full
         if (session.participant) return
-        res.status(400).json({ message: "session is full" })
+        res.status(409).json({ message: "session is full" })
 
         session.participant = userId
         await session.save()
@@ -145,9 +152,7 @@ export async function endSession(req, res) {
             return res.status(400).json({ message: "Session is already completed" })
         }
 
-        session.status = "completed"
-        await session.save()
-
+        
         const call = streamClient.video.call("default", session.callId)
         await call.delete({ hard: true })
 
@@ -155,6 +160,8 @@ export async function endSession(req, res) {
         const channel = chatClient.channel("messaging", session.callId)
         await channel.delete()
 
+        session.status = "completed"
+        await session.save()
 
 
         res.status(200).json({ session, message: "Session ended successfully" })
