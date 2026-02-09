8007173f49521aeac2fef4b809bfe46af2e70283
lakshmisanthosh87 <lakshmisanthosh8714@gmail.com>
2026-02-09 16:30:32 +0530

    create a sessionControllers functions

--- backend/src/lib/inngest.js
+++ backend/src/lib/inngest.js
@@ -1,6 +1,6 @@
 import { Inngest } from "inngest"
 import { connectDB } from "./db.js"
-import User from "../modules/User.js"
+import User from "../models/User.js"
 import { deleteStreamUser } from "./stream.js"
 import { upsertStreamUser } from './stream.js'
 
